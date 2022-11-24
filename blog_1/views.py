dd039cebd49d368b5c7523ea988e0f7722077d8f
Andy <andythomas.py@gmail.com>
2022-11-24 11:56:08 -0600

    Initial Commit

---
+++ blog_1/views.py
@@ -0,0 +1,52 @@
+from django.shortcuts import render
+import my_site_1.dummy_data as dummy_data
+
+
+def helpers(a):
+    return a['date']
+
+
+# Create your views here.
+
+def starting_page(request):
+    my_posts = dummy_data.posts
+
+
+    sorted_list = sorted(my_posts, key=helpers)
+
+    latest_posts = sorted_list[-3:]
+
+    for post in latest_posts:
+        print(post['date'])
+        print(type(post['image']))
+    
+    return render(request, "blog_1/index.html",
+    {
+        "posts": latest_posts
+    })
+
+
+def posts(request):
+    my_posts = dummy_data.posts
+    sorted_list = sorted(my_posts, key=helpers, reverse=True)
+
+    return render(request, "blog_1/all-posts.html",
+    {
+        "all_posts": sorted_list
+    })
+
+
+def post_detail(request, slug):
+    my_posts = dummy_data.posts
+
+    print(slug)
+
+    identified_post = next(item for item in my_posts if item["slug"] == slug)
+    print(identified_post)
+
+
+    return render(request, "blog_1/post-detail.html", 
+    {
+        "single_post": identified_post
+    })
+
---
+++ my_site_1/dummy_data.py
@@ -0,0 +1,110 @@
+from datetime import date 
+
+posts = [
+    {
+        "slug": "hike-in-the-mountains-1",
+        "image": "mountain3.jpeg",
+        "author": "Andy Thomas",
+        "date": date.today(),
+        "title": "Mountain Hiking 1",
+        "excerpt": """111There's nothing like the views you get when hiking in the 
+                    mountains! You'll never believe what I saw while enjoying
+                    the view!""",
+        "content": """111Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed quaerat commodi alias, 
+                    vero repellat distinctio dignissimos ullam maxime incidunt voluptate, exercitationem 
+                    officiis! Iste corrupti odio sunt voluptatibus non, iure aliquam!
+                    
+                    111Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed quaerat commodi alias, 
+                    vero repellat distinctio dignissimos ullam maxime incidunt voluptate, exercitationem 
+                    officiis! Iste corrupti odio sunt voluptatibus non, iure aliquam!
+                    
+                    111Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed quaerat commodi alias, 
+                    vero repellat distinctio dignissimos ullam maxime incidunt voluptate, exercitationem 
+                    officiis! Iste corrupti odio sunt voluptatibus non, iure aliquam!"""
+    },
+    {
+        "slug": "hike-in-the-mountains-2",
+        "image": "mountain3.jpeg",
+        "author": "Andy Thomas",
+        "date": date(2020, 8, 5),
+        "title": "Mountain Hiking 2",
+        "excerpt": """There's nothing like the views you get when hiking in the 
+                    mountains! You'll never believe what I saw while enjoying
+                    the view!""",
+        "content": """222Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed quaerat commodi alias, 
+                    vero repellat distinctio dignissimos ullam maxime incidunt voluptate, exercitationem 
+                    officiis! Iste corrupti odio sunt voluptatibus non, iure aliquam!
+                    
+                    222Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed quaerat commodi alias, 
+                    vero repellat distinctio dignissimos ullam maxime incidunt voluptate, exercitationem 
+                    officiis! Iste corrupti odio sunt voluptatibus non, iure aliquam!
+                    
+                    222Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed quaerat commodi alias, 
+                    vero repellat distinctio dignissimos ullam maxime incidunt voluptate, exercitationem 
+                    officiis! Iste corrupti odio sunt voluptatibus non, iure aliquam!"""
+    },
+    {
+        "slug": "hike-in-the-mountains-3",
+        "image": "mountain3.jpeg",
+        "author": "Andy Thomas",
+        "date": date(2021, 7, 21),
+        "title": "Mountain Hiking 3",
+        "excerpt": """There's nothing like the views you get when hiking in the 
+                    mountains! You'll never believe what I saw while enjoying
+                    the view!""",
+        "content": """333Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed quaerat commodi alias, 
+                    vero repellat distinctio dignissimos ullam maxime incidunt voluptate, exercitationem 
+                    officiis! Iste corrupti odio sunt voluptatibus non, iure aliquam!
+                    
+                    333Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed quaerat commodi alias, 
+                    vero repellat distinctio dignissimos ullam maxime incidunt voluptate, exercitationem 
+                    officiis! Iste corrupti odio sunt voluptatibus non, iure aliquam!
+                    
+                    333Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed quaerat commodi alias, 
+                    vero repellat distinctio dignissimos ullam maxime incidunt voluptate, exercitationem 
+                    officiis! Iste corrupti odio sunt voluptatibus non, iure aliquam!"""
+    },    
+    {
+        "slug": "hike-in-the-mountains-4",
+        "image": "giraffe.jpeg",
+        "author": "Andy Thomas",
+        "date": date.today(),
+        "title": "Mountain Hiking 4",
+        "excerpt": """2222There's nothing like the views you get when hiking in the 
+                    mountains! You'll never believe what I saw while enjoying
+                    the view!""",
+        "content": """444Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed quaerat commodi alias, 
+                    vero repellat distinctio dignissimos ullam maxime incidunt voluptate, exercitationem 
+                    officiis! Iste corrupti odio sunt voluptatibus non, iure aliquam!
+                    
+                    444Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed quaerat commodi alias, 
+                    vero repellat distinctio dignissimos ullam maxime incidunt voluptate, exercitationem 
+                    officiis! Iste corrupti odio sunt voluptatibus non, iure aliquam!
+                    
+                    444Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed quaerat commodi alias, 
+                    vero repellat distinctio dignissimos ullam maxime incidunt voluptate, exercitationem 
+                    officiis! Iste corrupti odio sunt voluptatibus non, iure aliquam!"""
+    },
+    {
+        "slug": "hike-in-the-mountains-5",
+        "image": "space.jpeg",
+        "author": "Andy Thomas",
+        "date": date.today(),
+        "title": "Mountain Hiking 5",
+        "excerpt": """333There's nothing like the views you get when hiking in the 
+                    mountains! You'll never believe what I saw while enjoying
+                    the view!""",
+        "content": """555Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed quaerat commodi alias, 
+                    vero repellat distinctio dignissimos ullam maxime incidunt voluptate, exercitationem 
+                    officiis! Iste corrupti odio sunt voluptatibus non, iure aliquam!
+                    
+                    555Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed quaerat commodi alias, 
+                    vero repellat distinctio dignissimos ullam maxime incidunt voluptate, exercitationem 
+                    officiis! Iste corrupti odio sunt voluptatibus non, iure aliquam!
+                    
+                    555Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed quaerat commodi alias, 
+                    vero repellat distinctio dignissimos ullam maxime incidunt voluptate, exercitationem 
+                    officiis! Iste corrupti odio sunt voluptatibus non, iure aliquam!"""
+    },
+
+]
