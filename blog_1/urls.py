e293d58c263d19f1ccf1ad14709a6d2f71ab066c
Andy <andythomas.py@gmail.com>
2022-12-06 17:06:54 -0600

    replace functions with classes

--- blog_1/urls.py
+++ blog_1/urls.py
@@ -3,8 +3,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.starting_page, name="starting_page"),
-    path("posts/", views.posts, name="posts-page"),
-    path("posts/<slug:slug>", views.post_detail, 
+    path("", views.IndexView.as_view(), name="starting_page"),
+    path("posts/", views.AllPostsView.as_view(), name="posts-page"),
+    path("posts/<slug:slug>", views.PostDetailView.as_view(), 
         name="post-detail-page"), #/posts/my-first-post   slug is search engine indentifier
 ]
--- blog_1/views.py
+++ blog_1/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render, get_object_or_404
-from .models import Post
+from django.views.generic.list import ListView
+from django.views.generic import DetailView
+
+from .models import Post, Tag
+
 # import my_site_1.dummy_data as dummy_data
 
 
@@ -10,61 +14,75 @@ def helpers_db(a):
     return a.date
 
 
+
 # Create your views here.
 
-def starting_page(request):
-    # my_posts = dummy_data.posts
-    # print(my_posts)
-    my_posts_db = Post.objects.all().order_by("-date")[:3]
-    
-    for post in my_posts_db:
-        print(post.date)
+# def starting_page(request):
+#     my_posts_db = Post.objects.all().order_by("-date")[:3]
 
-    print("-------------------------")
+#     return render(request, "blog_1/index.html",
+#     {
+#         "posts_db": my_posts_db
+#     })
 
-    # sorted_list = sorted(my_posts, key=helpers)
-    # sorted_list_db = sorted(my_posts_db, key=helpers_db)
+class IndexView(ListView):
+    template_name = "blog_1/index.html"
+    model = Post
+    ordering = ["-date"]
+    context_object_name = "posts"       # Since we are using "ListView", we can access "object_list" (by default) will send data to index.html
 
-    # latest_posts = sorted_list[-3:]
-    # latest_posts_db = my_posts_db[-3:]
-    # print(latest_posts_db)
-
-    
-    return render(request, "blog_1/index.html",
-    {
-        # "posts": latest_posts,
-        "posts_db": my_posts_db
-    })
+    def get_queryset(self):
+        query_set = super().get_queryset()
+        data = query_set[:3]
+        return data
 
 
-def posts(request):
-    # my_posts = dummy_data.posts
-    my_posts_db = Post.objects.all().order_by('-date')
-    # sorted_list = sorted(my_posts, key=helpers, reverse=True)
-    # sorted_list_db = sorted(my_posts_db, key=helpers_db) 
-
-    return render(request, "blog_1/all-posts.html",
-    {
-        "all_posts": my_posts_db
-    })
+class AllPostsView(ListView):
+    template_name = "blog_1/all-posts.html"
+    model = Post
+    ordering = ["-date"]
+    context_object_name = "all_posts"
 
 
-def post_detail(request, slug):
-    # my_posts = dummy_data.posts
-    # my_posts_db = Post.objects.all()
-    # identified_post_db = Post.objects.get(slug=slug)
-    identified_post_db = get_object_or_404(Post, slug=slug)
-    print(identified_post_db.author, identified_post_db.date)
-    print(identified_post_db.tags.all())
-    # identified_post = next(item for item in my_posts if item["slug"] == slug)
-    # identified_post_db = next(item for item in my_posts_db if item.slug == slug)
+
+# def posts(request):
+#     # my_posts = dummy_data.posts
+#     my_posts_db = Post.objects.all().order_by('-date')
+#     # sorted_list = sorted(my_posts, key=helpers, reverse=True)
+#     # sorted_list_db = sorted(my_posts_db, key=helpers_db) 
+
+#     return render(request, "blog_1/all-posts.html",
+#     {
+#         "all_posts": my_posts_db
+#     })
 
 
-    return render(request, "blog_1/post-detail.html", 
-    {
-        # "single_post": identified_post,
-        "single_post": identified_post_db,
-        "tags": identified_post_db.tags.all()
-    })
+class PostDetailView(DetailView):
+    template_name = "blog_1/post-detail.html"
+    model = Post            # With subclass DetailView, Django will automatically search by primary key or slug(which we are using here in urls.py)
+    context_object_name = "single_post"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["get_tags"] = self.object.tags.all()
+        return context
+
+# def post_detail(request, slug):
+#     # my_posts = dummy_data.posts
+#     # my_posts_db = Post.objects.all()
+#     # identified_post_db = Post.objects.get(slug=slug)
+#     identified_post_db = get_object_or_404(Post, slug=slug)
+#     print(identified_post_db.author, identified_post_db.date)
+#     print(identified_post_db.tags.all())
+#     # identified_post = next(item for item in my_posts if item["slug"] == slug)
+#     # identified_post_db = next(item for item in my_posts_db if item.slug == slug)
+
+
+#     return render(request, "blog_1/post-detail.html", 
+#     {
+#         # "single_post": identified_post,
+#         "single_post": identified_post_db,
+#         "tags": identified_post_db.tags.all()
+#     })
 
 
