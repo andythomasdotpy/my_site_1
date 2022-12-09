7250688d0b5354f38e6d32fe7ff39f04362eac62
Andy <andythomas.py@gmail.com>
2022-12-08 22:55:22 -0600

    view.py with and without context function

--- blog_1/views.py
+++ blog_1/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render, get_object_or_404
 from django.views.generic.list import ListView
-from django.views.generic import DetailView
+from django.urls import reverse
+from django.views.generic import DetailView, View
+from django.http import HttpResponseRedirect
 
-from .models import Post, Tag
+from .models import Post, Tag, Comment
 from .forms import CommentForm
 
 # import my_site_1.dummy_data as dummy_data
@@ -36,16 +38,57 @@ class AllPostsView(ListView):
 
 
 
-class PostDetailView(DetailView):
-    template_name = "blog_1/post-detail.html"
-    model = Post            # With subclass DetailView, Django will automatically search by primary key or slug(which we are using here in urls.py)
-    context_object_name = "single_post"
+# class PostDetailView(DetailView):
+#     template_name = "blog_1/post-detail.html"
+#     model = Post            # With subclass DetailView, Django will automatically search by primary key or slug(which we are using here in urls.py)
+#     context_object_name = "single_post"
 
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context["get_tags"] = self.object.tags.all()
-        context["comment_form"] = CommentForm()
+#     def get_context_data(self, **kwargs):
+#         context = super().get_context_data(**kwargs)
+#         context["get_tags"] = self.object.tags.all()
+#         context["comment_form"] = CommentForm()
+#         return context
+
+
+# Now we create manual view to create our own logic
+class PostDetailView(View):
+    def func_context(self, slug):
+        post = Post.objects.get(slug=slug)
+        context = {
+            "single_post": post,
+            "get_tags": post.tags.all(),
+            "comment_form": CommentForm(),
+            "comments": post.comments.all().order_by("-id"),     # in models.py we used related_name="comments" for post column
+        }
         return context
 
+
+    def get(self, request, slug):
+        # post = Post.objects.get(slug=slug)
+        context = self.func_context(slug)
+        return render(request, "blog_1/post-detail.html", context)
+
+
+    def post(self, request, slug):
+        comment_form = CommentForm(request.POST)
+        post = Post.objects.get(slug=slug)
+        print(post)
+
+        if comment_form.is_valid():
+            comment = comment_form.save(commit=False)      # commit equals false will prevent saving to the database but will create a new model instance, since we have exclded post (in form) we need to link it to post before saving
+            comment.post = post     # now we set comment post = post of the slug for which the view was reached in the end (see a few lines above)
+            comment.save()   # now that we've attached post we can save to database
+
+            return HttpResponseRedirect(reverse("post-detail-page", args=[slug]))
+
+        context = {
+            "single_post": post,
+            "get_tags": post.tags.all(),
+            "comment_form": comment_form,
+            "comments": post.comments.all().order_by("-id"),    # we also place comments here in the case of failed validation and post request is reloaided
+        }
+
+        return render(request, "blog_1/post-detail.html", context)
+    
 
 
